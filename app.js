1809e598c95f8e95d1d52423efd2a2e989fa3cbe
unknown <drastic87@gmail.com>
2021-03-16 21:12:42 -0400

    Stage 2 security using mongoose encrypt

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require('ejs');
 const port = 3000;
 const app = express();
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -12,10 +13,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser:true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema);
 

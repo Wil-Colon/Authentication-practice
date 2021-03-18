603a0f9f46f612afefee88b7555696d243e60458
unknown <drastic87@gmail.com>
2021-03-18 18:47:32 -0400

    level 3 with Hashing paswords

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require('ejs');
 const port = 3000;
 const app = express();
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 console.log(process.env.API_KEY);
 
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
 
 const User = new mongoose.model('User', userSchema);
 
@@ -42,7 +42,7 @@ app.get('/register', (req,res) => {
 app.post('/register', (req,res) => {
   const newUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save((err) => {
     if(err) {
@@ -58,7 +58,7 @@ app.post('/login', (req,res) => {
   User.findOne({email:req.body.username}, (err, foundUser) => {
     if (!err){
       if(foundUser){
-        if(foundUser.password === req.body.password) {
+        if(foundUser.password === md5(req.body.password)) {
           res.render('secrets');
         }else{
           res.send('<h1> Wrong password </h1>');
